949fd695563d12859c35a4bc2432ae1b3f81ae27
Maulana Adil <maulanaadil.latif@gmail.com>
2022-06-26 23:36:27 +0700

    implement feature create post

--- app/services/posts.server.ts
+++ app/services/posts.server.ts
@@ -1,5 +1,15 @@
+import type { Post } from '@prisma/client';
 import { db } from './db.server';
 
 export type { Post } from '@prisma/client';
 
 export const getPosts = () => db.post.findMany();
+
+export const createPost = ({ title, body }: Pick<Post, 'title' | 'body'>) => {
+  return db.post.create({
+    data: {
+      title,
+      body,
+    },
+  });
+};
