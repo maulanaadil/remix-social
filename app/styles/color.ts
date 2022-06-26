3f4107253814a7720d92af486aec307910ffbac9
Maulana Adil <maulanaadil.latif@gmail.com>
2022-06-26 23:35:56 +0700

    add color pallete into project

---
+++ app/styles/color.ts
@@ -0,0 +1,18 @@
+const color = {
+  white: '#fafafa',
+  gray: {
+    gray1: '#a3a3a3',
+    gray2: '#66676B',
+    gray3: '#4C4C4C',
+  },
+  blue: {
+    primary: '#73A8E6',
+    secondary: '#90E0F7',
+  },
+  darkBlue: {
+    primary: '#2671CC',
+    secondary: '#54C6E6',
+  },
+};
+
+export default color;
